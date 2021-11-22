096711713478ec138b655d1f0791d7ba9b66c804
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2021-11-22 11:29:15 -0500

    Refactored

---
+++ modules/function/variables.tf
@@ -0,0 +1,19 @@
+variable "source_path" {
+  type = string
+}
+
+variable "role_arn" {
+  type = string
+}
+
+variable "bucket_id" {
+  type = string
+}
+
+variable "gateway_id" {
+  type = string
+}
+
+variable "gateway_execution_arn" {
+  type = string
+}
